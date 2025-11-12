6a6b241f96ab0dff447774dc97d7fd31151c41d8
yash79990 <yash.awasthi_cs23@gla.ac.in>
2025-11-12 13:18:28 +0530

    Remove comments and clean up random math quiz code

--- random_math_quiz.js
+++ random_math_quiz.js
@@ -1,18 +1,16 @@
- // Q9. Random Math Quiz Generator
 
-// Generate two random numbers between 1 and 20
+
+
 let num1 = Math.floor(Math.random() * 20) + 1;
 let num2 = Math.floor(Math.random() * 20) + 1;
 
-// List of possible arithmetic operators
+
 const operators = ['+', '-', '*', '/'];
 
-// Randomly select one operator
 let randomOperator = operators[Math.floor(Math.random() * operators.length)];
 
 let correctAnswer;
 
-// Use switch to calculate the correct answer based on operator
 switch (randomOperator) {
   case '+':
     correctAnswer = num1 + num2;
@@ -27,7 +25,7 @@ switch (randomOperator) {
     break;
 
   case '/':
-    // Round division to 2 decimal places
+    
     correctAnswer = (num1 / num2).toFixed(2);
     break;
 
